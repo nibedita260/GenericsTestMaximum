e7624604a007cbf318d7a080dbd375a73331bc13
nibedita260 <deepi.nibedita260.com>
2021-09-30 22:14:32 +0530

    [Nibedita]:Added FindMaxStrings in GenericsTextMaximum

--- GenericsTestMaximum/Program.cs
+++ GenericsTestMaximum/Program.cs
@@ -7,8 +7,12 @@ namespace GenericsTestMaximum
         static void Main(string[] args)
         {
             FindMaximum findMaximum = new FindMaximum();
-            findMaximum.FindMax(4,11,5);
-            findMaximum.FindMaxFloats(2.5F, 2.2F, 2.8F);
+            int x=findMaximum.FindMax(4,11,5);
+            Console.WriteLine(x);
+            float y=findMaximum.FindMax(2.5F, 2.2F, 2.8F);
+            Console.WriteLine(y);
+            string z=findMaximum.FindMax("Apple","Peach","Banana");
+            Console.WriteLine(z);
         }
     }
 }
