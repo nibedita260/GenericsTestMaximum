d4695925065ababf072e376b4c84f0bc2e1023fd
nibedita260 <deepi.nibedita260.com>
2021-09-30 21:38:38 +0530

    [Nibedita]:Added FindMaxFloats in GenericsTextMaximum

--- GenericsTestMaximum/FindMaximum.cs
+++ GenericsTestMaximum/FindMaximum.cs
@@ -23,6 +23,27 @@ namespace GenericsTestMaximum
                 max = n3;
             }
             Console.Write(max);
+            Console.WriteLine(" ");
+            return max;
+        }
+        public float FindMaxFloats(float n1, float n2, float n3)
+        {
+            float max = 0;
+            Console.Write("The maximum among three numbers:");
+            if (n1.CompareTo(n2) > 0 && n1.CompareTo(n3) > 0)
+            {
+                max = n1;
+            }
+            if (n2.CompareTo(n1) > 0 && n2.CompareTo(n3) > 0)
+            {
+                max = n2;
+            }
+            if (n3.CompareTo(n1) > 0 && n3.CompareTo(n2) > 0)
+            {
+                max = n3;
+            }
+            Console.Write(max);
+            Console.WriteLine(" ");
             return max;
         }
     }
--- GenericsTestMaximum/Program.cs
+++ GenericsTestMaximum/Program.cs
@@ -8,6 +8,7 @@ namespace GenericsTestMaximum
         {
             FindMaximum findMaximum = new FindMaximum();
             findMaximum.FindMax(4,11,5);
+            findMaximum.FindMaxFloats(2.5F, 2.2F, 2.8F);
         }
     }
 }
