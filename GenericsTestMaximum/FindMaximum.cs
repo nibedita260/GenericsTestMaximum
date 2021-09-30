01e62b8a193c5fa709a6f0b99500b7562d5ae4ed
nibedita260 <deepi.nibedita260.com>
2021-09-30 21:28:24 +0530

    [Nibedita]:Added FindMaxNumber in GenericsTextMaximum

---
+++ GenericsTestMaximum/FindMaximum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsTestMaximum
+{
+    class FindMaximum
+    {
+        public int FindMax(int n1,int n2,int n3)
+        {
+            int max = 0;
+            Console.Write("The maximum among three numbers:");
+            if(n1.CompareTo(n2)>0 && n1.CompareTo(n3)>0)
+            {
+                max = n1;
+            }
+            if (n2.CompareTo(n1) > 0 && n2.CompareTo(n3) > 0)
+            {
+                max = n2;
+            }
+            if (n3.CompareTo(n1) > 0 && n3.CompareTo(n2) > 0)
+            {
+                max = n3;
+            }
+            Console.Write(max);
+            return max;
+        }
+    }
+}
---
+++ GenericsTestMaximum/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericsTestMaximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindMaximum findMaximum = new FindMaximum();
+            findMaximum.FindMax(4,11,5);
+        }
+    }
+}
