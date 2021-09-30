23faa88a6da4ea3abef49214c42457a940d4c8de
nibedita260 <deepi.nibedita260.com>
2021-09-30 22:19:21 +0530

    [Nibedita]:Added FindMaxFloats in GenericsTextMaximum

--- GenericsTestMaximum/FindMaximum.cs
+++ GenericsTestMaximum/FindMaximum.cs
@@ -6,45 +6,27 @@ namespace GenericsTestMaximum
 {
     class FindMaximum
     {
-        public int FindMax(int n1,int n2,int n3)
+        public int FindMax(int n1, int n2, int n3)
         {
-            int max = 0;
-            Console.Write("The maximum among three numbers:");
-            if(n1.CompareTo(n2)>0 && n1.CompareTo(n3)>0)
-            {
-                max = n1;
-            }
-            if (n2.CompareTo(n1) > 0 && n2.CompareTo(n3) > 0)
-            {
-                max = n2;
-            }
-            if (n3.CompareTo(n1) > 0 && n3.CompareTo(n2) > 0)
-            {
-                max = n3;
-            }
-            Console.Write(max);
-            Console.WriteLine(" ");
-            return max;
-        }
-        public float FindMaxFloats(float n1, float n2, float n3)
-        {
-            float max = 0;
             Console.Write("The maximum among three numbers:");
             if (n1.CompareTo(n2) > 0 && n1.CompareTo(n3) > 0)
-            {
-                max = n1;
-            }
+                return n1;
             if (n2.CompareTo(n1) > 0 && n2.CompareTo(n3) > 0)
-            {
-                max = n2;
-            }
+                return n2;
             if (n3.CompareTo(n1) > 0 && n3.CompareTo(n2) > 0)
-            {
-                max = n3;
-            }
-            Console.Write(max);
-            Console.WriteLine(" ");
-            return max;
+                return n3;
+            return 0;
+        }
+        public float FindMax(float n1, float n2, float n3)
+        {
+            Console.Write("The maximum among three floats:");
+            if (n1.CompareTo(n2) > 0 && n1.CompareTo(n3) > 0)
+                return n1;
+            if (n2.CompareTo(n1) > 0 && n2.CompareTo(n3) > 0)
+                return n2;
+            if (n3.CompareTo(n1) > 0 && n3.CompareTo(n2) > 0)
+                return n3;
+            return 0.0f;
         }
     }
 }
