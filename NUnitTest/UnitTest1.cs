bf4659f17adedf0a689c38290bf58c445f490852
nibedita260 <deepi.nibedita260.com>
2021-10-03 19:38:22 +0530

    [Nibedita]:Added UnitTest For GenericMaximumStrings

--- NUnitTest/UnitTest1.cs
+++ NUnitTest/UnitTest1.cs
@@ -9,26 +9,9 @@ namespace NUnitTest
         [Test]
         public void Test1()
         {
-            int[] intArray = { 4, 11, 7 };
-            int max = 11;
-            FindMaximum<int> findMaximum = new FindMaximum<int>(intArray);
-            int res=findMaximum.FindMax();
-            Assert.AreEqual(res,max);
-        }
-        public void Test2()
-        {
-            float[] floatArray = { 2.5f, 2.2f, 2.8f };
-            float max = 2.8f;
-            FindMaximum<float> findMaximum = new FindMaximum<float>(floatArray);
-            float res = findMaximum.FindMax();
-            Assert.AreEqual(res, max);
-        }
-        public void Test3()
-        {
-            string[] stringArray = { "Apple", "Peach", "Banana" };
             string max = "Peach";
-            FindMaximum<string> findMaximum = new FindMaximum<string>(stringArray);
-            string res = findMaximum.FindMax();
+            FindMaximum findMaximum = new FindMaximum();
+            string res = findMaximum.FindMax("Apple", "Peach", "Banana");
             Assert.AreEqual(res, max);
         }
     }
