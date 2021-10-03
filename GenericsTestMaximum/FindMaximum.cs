eda8b50ea17af860fb6504761fbbe6b85e8d8b12
nibedita260 <deepi.nibedita260.com>
2021-10-03 19:41:54 +0530

    [Nibedita]:Added UnitTest For GenericMaximumNumbers

--- GenericsTestMaximum/FindMaximum.cs
+++ GenericsTestMaximum/FindMaximum.cs
@@ -17,16 +17,5 @@ namespace GenericsTestMaximum
                 return n3;
             return 0;
         }
-        public float FindMax(float n1, float n2, float n3)
-        {
-            Console.Write("The maximum among three floats:");
-            if (n1.CompareTo(n2) > 0 && n1.CompareTo(n3) > 0)
-                return n1;
-            if (n2.CompareTo(n1) > 0 && n2.CompareTo(n3) > 0)
-                return n2;
-            if (n3.CompareTo(n1) > 0 && n3.CompareTo(n2) > 0)
-                return n3;
-            return 0.0f;
-        }
     }
 }
--- GenericsTestMaximum/Program.cs
+++ GenericsTestMaximum/Program.cs
@@ -7,8 +7,8 @@ namespace GenericsTestMaximum
         static void Main(string[] args)
         {
             FindMaximum findMaximum = new FindMaximum();
-            float y=findMaximum.FindMax(2.5F, 2.2F, 2.8F);
-            Console.WriteLine(y);
+            int x=findMaximum.FindMax(4,11,5);
+            Console.WriteLine(x);
         }
     }
 }
