28497d37ca9ce0f7e9e517a0fe2dc17584a444a5
nibedita260 <deepi.nibedita260.com>
2021-10-03 17:15:15 +0530

    [Nibedita]:Added UnitTest For MaximumStrings

--- GenericsTestMaximum/FindMaximum.cs
+++ GenericsTestMaximum/FindMaximum.cs
@@ -9,11 +9,7 @@ namespace GenericsTestMaximum
         public int FindMax(int n1, int n2, int n3)
         {
             Console.Write("The maximum among three numbers:");
-<<<<<<< HEAD
-            if(n1.CompareTo(n2)>0 && n1.CompareTo(n3)>0)
-=======
             if (n1.CompareTo(n2) > 0 && n1.CompareTo(n3) > 0)
->>>>>>> UC2-MaximumFloats
                 return n1;
             if (n2.CompareTo(n1) > 0 && n2.CompareTo(n3) > 0)
                 return n2;
@@ -24,7 +20,6 @@ namespace GenericsTestMaximum
         public float FindMax(float n1, float n2, float n3)
         {
             Console.Write("The maximum among three floats:");
-<<<<<<< HEAD
             if (n1.CompareTo(n2) > 0 && n1.CompareTo(n3) > 0)
                 return n1;
             if (n2.CompareTo(n1) > 0 && n2.CompareTo(n3) > 0)
@@ -36,19 +31,13 @@ namespace GenericsTestMaximum
         public string FindMax(string n1, string n2, string n3)
         {
             Console.Write("The maximum among three string:");
-=======
->>>>>>> UC2-MaximumFloats
             if (n1.CompareTo(n2) > 0 && n1.CompareTo(n3) > 0)
                 return n1;
             if (n2.CompareTo(n1) > 0 && n2.CompareTo(n3) > 0)
                 return n2;
             if (n3.CompareTo(n1) > 0 && n3.CompareTo(n2) > 0)
                 return n3;
-<<<<<<< HEAD
             return null;
-=======
-            return 0.0f;
->>>>>>> UC2-MaximumFloats
         }
     }
 }
